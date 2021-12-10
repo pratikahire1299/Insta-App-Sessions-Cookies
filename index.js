183d6df77b74e7b5352e81a0eec0f3203fce55dc
Pratik Ahire <pratikahire522@gmail.com>
2021-12-10 14:47:31 +0530

    Second  commit

--- index.js
+++ index.js
@@ -3,20 +3,41 @@ const app = express();
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const PORT = 5000;
+const bcrypt = require("bcrypt");
+
+const dbconnection = require("./DbConnection.js");
+const User = require("./models/userdetails");
 
 app.use(cookieParser());
-app.use(session({ secret: "mano1234", saveUninitialized: true, resave: true }));
+app.use(express.json());
+app.use(session({ secret: "gfdtr45MySeceret47uybgdrsj", saveUninitialized: true, resave: true }));
 
 const useer = {
-    name: "ManodddSriram",
-    source: "Youtube"
+    name: "Ramesh",
+    source: "123"
 };
 
 // console.log(process.env);
-app.get("/login", (req, res) => {
-    req.session.user = useer;
-    req.session.save();
-    return res.send("User logged in");
+app.post("/login", async (req, res) => {
+    try {
+  
+        const User_Name= req.body.User_Name;
+        const Password=req.body.Password;
+        if (!(User_Name && Password)) {
+          res.status(400).send("Please Enter Valid Inputs");
+        }
+        const user = await User.findOne({ User_Name });
+        if (user && (await bcrypt.compare(Password, user.Password))) {
+    
+            req.session.user = User_Name;
+            req.session.save();
+            return res.send("User logged in");
+        }else{
+        res.status(400).send("Invalid Credentials || Wrong username or password");}
+      } catch (err) {
+        console.log(err);
+      }
+
 });
 
 app.get("/user", (req, res) => {
@@ -30,3 +51,6 @@ app.get("/logout", (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server at ${PORT}`));
+
+
+
