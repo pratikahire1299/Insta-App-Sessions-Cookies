31dd7df50bf97f732e16c9a504fd2cac4e88a51b
Pratik Ahire <pratikahire522@gmail.com>
2022-01-16 11:52:25 +0530

    ninth commit

--- models/userdetails.js
+++ models/userdetails.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
-  name: { type: String, default: '' },
-  user_name: { type: String, default: '' },
+  name: { type: String, default: 'null' },
+  user_name: { type: String, default: 'null' },
   contact_number: { type: Number, default: 0 },
   birthdate: { type: String, format: 'date' },
-  password: { type: String, default: '' },
+  password: { type: String, default: 'null' },
   profile_image: { type: String, default: 'null' },
 });
 
